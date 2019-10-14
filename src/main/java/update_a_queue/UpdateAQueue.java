e6c216aea91bba3b73f6fcc2bc708c8bd38156f9
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-14 11:07:35 -0500

    Initial commit

---
+++ src/main/java/update_a_queue/UpdateAQueue.java
@@ -0,0 +1,35 @@
+package main.java.update_a_queue;
+
+import com.vailsys.persephony.api.PersyClient;
+import com.vailsys.persephony.api.PersyException;
+import com.vailsys.persephony.api.queue.Queue;
+import com.vailsys.persephony.api.queue.QueueUpdateOptions;
+
+public class UpdateAQueue {
+
+  public static void main(String[] args) {
+    String accountId = System.getenv("ACCOUNT_ID");
+    String authToken = System.getenv("AUTH_TOKEN");
+    String queueId = "QU4fe17b9d24c35cc45479e37ab89605a88968a373";
+
+    updateAQueue(queueId, accountId, authToken);
+  }
+
+  public static void updateAQueue(String queueId, String accountId, String authToken) {
+    try {
+      // Create PersyClient object
+      PersyClient client = new PersyClient(accountId, authToken);
+
+      // Options payload to change the alias and payload of the specified queue
+      QueueUpdateOptions options = new QueueUpdateOptions();
+      options.setAlias("Updated Alias");
+      options.setMaxSize(12);
+
+      // Invoke update method to modify the queue
+      Queue queue = client.queues.update(queueId, options);
+
+    } catch (PersyException e) {
+      // Exception throw upon failure
+    }
+  }
+}
